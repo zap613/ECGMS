e65180afa1563a02ecb46a3ed241a62b65ec4dae
vtavta30920 <156265336+vtavta30920@users.noreply.github.com>
2025-10-08 00:44:49 +0700

    add tailwind + postcss

---
+++ src/tailwind.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Quét toàn bộ file React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
